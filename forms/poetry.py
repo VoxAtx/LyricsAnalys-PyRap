fbcf83e52724d497bc72edeb355cb9a54a10e14a
VoxAtx <Voxatx@proton.me>
2020-10-14 23:08:19 +0000

    prepare

--- forms/poetry.py
+++ forms/poetry.py
@@ -2,3 +2,19 @@
 #! /usr/bin/env python
 from line import Line
 from section import Section
+
+
+class Haiku(Section):
+    '''
+    3 Lines with 5/7/5 syllable counts
+    '''
+    def __init__(self, lines=[], name='', topic=''):
+
+        if not lines:
+            self.lines = [Line(), Line(), Line()]
+        else:
+            if len(lines) != 3:
+                raise ValueError("Error: Must be 3 lines")
+            self.lines = [Line(x) for x in lines]
+        self.name = name
+        self.topic = topic
