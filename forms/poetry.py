42bf644f0274a9c03ab926c1d3ca6d6b32180af1
VoxAtx <Voxatx@proton.me>
2020-10-21 13:02:08 +0000

    a bunch of stuff

--- forms/poetry.py
+++ forms/poetry.py
@@ -29,3 +29,19 @@ class Limerick(Section):
     5 Lines with 5/7/5 syllable counts
     '''
     def __init__(self, lines=[], name='', topic=''):
+
+        if not lines:
+            self.lines = [Line(), Line(), Line(), Line(), Line()]
+        else:
+            if len(lines) != 5:
+                raise ValueError("Error: Must be 3 lines")
+            self.lines = [Line(x) for x in lines]
+        self.name = name
+        self.topic = topic
+        self.description = "5 lines with 5/7/5 syllable counts\n\
+                            Rhythm pattern is (weak: dashes, strong: backslash):\n\
+                            1) -/--/--/\n\
+                            2) -/--/--/\n\
+                            3) -/--/\n\
+                            4) -/--/\n\
+                            5) -/--/--/\n\
