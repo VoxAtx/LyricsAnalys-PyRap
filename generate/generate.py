80e8e91e00bccea85fc36a3d018186e355baab96
VoxAtx <Voxatx@proton.me>
2020-12-30 13:00:09 +0000

    Dummy

--- generate/generate.py
+++ generate/generate.py
@@ -15,3 +15,22 @@ from util.wordutil import WordUtil as WU
 class Generate:
 
     def __init__(self):
+        self.pos_dict = {}
+        self.topic_dict = {}
+        self.topic_words = []
+        self.line_topics = {}
+        self.lines = []
+        self.conjunctions = ['and', 'but', 'yet', 'or', 'because',
+                            'for', 'nor', 'so', 'rather than',
+                            'as much as']
+
+    def pairs(self, n_pairs=100, topic=None):
+        '''
+        Generate pairs of words using the
+        formula `pair = topic_word + random[adj, adv, verb]`
+
+        Parameters
+        -------
+        n_pairs: int
+            - Number of pairs to generate
+        '''
