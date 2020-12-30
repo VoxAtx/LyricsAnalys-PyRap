40d841f0b3c485d805fcfb9a7cd1603bc039eb70
VoxAtx <Voxatx@proton.me>
2020-12-30 10:28:24 +0000

    Commit missing files

--- generate/generate.py
+++ generate/generate.py
@@ -2,3 +2,16 @@
 import os
 import sys
 import random
+
+from PyDictionary import PyDictionary
+from nltk.corpus import wordnet as wn
+
+if '../' not in sys.path:
+    sys.path.append("../")
+
+from util.wordutil import WordUtil as WU
+
+
+class Generate:
+
+    def __init__(self):
