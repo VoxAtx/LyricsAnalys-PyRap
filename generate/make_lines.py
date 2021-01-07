120ec9069d10296ac634c8437255196dfea91194
VoxAtx <Voxatx@proton.me>
2021-01-07 18:24:17 +0000

    Version 0.0.0.1

--- generate/make_lines.py
+++ generate/make_lines.py
@@ -16,3 +16,20 @@ if __name__ == '__main__':
     once quit, save lines in 'savedlines/'
     '''
     conjunctions = ['and', 'but', 'yet', 'or', 'because',
+                        'for', 'nor', 'so', 'rather than',
+                        'as much as']
+
+    compares = ['is better than',
+              'is worse than',
+              'could never defeat a',
+              'would win in a fight with a',
+              'could never fuck a',
+              'always made me feel like a',
+              'could never be as good as a']
+
+    createstuff = Generate()
+    createstuff.load_lines()
+
+    lines = createstuff.lines
+
+    save_lines = []
