c33e6ad8191d136a4fa2cd92a53fc21888ede99c
VoxAtx <Voxatx@proton.me>
2021-01-16 06:20:47 +0000

    added video link

--- generate/make_pairs.py
+++ generate/make_pairs.py
@@ -9,3 +9,19 @@ from generate import Generate
 
 
 if __name__ == '__main__':
+    '''
+    y/n on word pairs consisting of
+    random([adj, adv, verb]) + TopicWord
+
+    once quit, save lines in 'saved/'
+
+    Parameters
+    -------
+    n_pairs: int
+        - Number of pairs to generate
+        - Ex: python make_pairs.py 420
+    '''
+    n_pairs = 100
+
+    if len(sys.argv) > 1:
+        n_pairs = int(sys.argv[1])
