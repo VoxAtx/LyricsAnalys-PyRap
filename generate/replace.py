ff56fc3b091dd596c75be6b839df99b2e5b5e9b8
VoxAtx <Voxatx@proton.me>
2021-01-17 10:20:42 +0000

    chore(deps): lock file maintenance

--- generate/replace.py
+++ generate/replace.py
@@ -10,3 +10,19 @@ if '../' not in sys.path:
 from genius.lyrics import ConvertLyrics as cl
 from util.wordutil import WordUtil as wu
 from generate import Generate as gen
+
+
+class Replace:
+
+    def __init__(self, artist=''):
+        self.r = wu()
+        self.c = cl()
+        self.artists = self.r.artists
+        if not artist:
+            artist = self.artists[random.randrange(0, len(self.artists))]
+        self.artist = artist
+
+    def replace_rap(self):
+        '''
+        Get topic and words and try to replace all tagged POS in songs
+        '''
