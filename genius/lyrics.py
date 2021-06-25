f5874a389bc80b671a147c3c74813de6d216f1c6
VoxAtx <Voxatx@proton.me>
2021-06-25 08:11:29 +0000

    Unused

--- genius/lyrics.py
+++ genius/lyrics.py
@@ -3,3 +3,13 @@ from nltk.corpus import wordnet
 import nltk
 
 import os
+import re
+import pickle
+import random
+
+
+class Lyrics:
+
+    def __init__(self):
+        self.lyrics_path = '../lyrics/'
+        self.artists = ['Kendrick Lamar', 'Drake', 'Chance The Rapper', 'J. Cole',
