221e0e555a52d336f8feed3c3674aef28d8fd876
VoxAtx <Voxatx@proton.me>
2021-06-30 10:57:13 +0000

    Updated ignores.

--- genius/lyrics.py
+++ genius/lyrics.py
@@ -13,3 +13,21 @@ class Lyrics:
     def __init__(self):
         self.lyrics_path = '../lyrics/'
         self.artists = ['Kendrick Lamar', 'Drake', 'Chance The Rapper', 'J. Cole',
+                        'Logic', 'Future', 'Chief Keef', 'Eminem', 'Kanye West',
+                        'JAY-Z', 'Big Sean', 'Lil Uzi Vert', 'Tyler, The Creator',
+                        'Earl Sweatshirt', '2 Chainz', 'G-Eazy', 'ScHoolboy Q',
+                        'Young Thug', 'Joey Bada$$', 'PnB Rock', 'Flatbush Zombies',
+                        'A$AP Rocky', 'A$AP Ferg', 'Dumbfoundead', 'Tory Lanez',
+                        'Waka Flocka Flame', 'Nas', 'A Tribe Called Quest', 'Vic Mensa',
+                        '$UICIDEBOY$', 'Denzel Curry', 'Maxo Kream', 'Isaiah Rashad',
+                        'Mike Stud', 'Mac Miller', 'Yonas', 'Childish Gambino', 'Rich Chigga',
+                        'Three 6 Mafia', 'Azizi Gibson', 'RiFF RAFF', 'Lil Dicky',
+                        'Lil Wayne', 'Tyga', 'Gucci Mane', 'Rick Ross', 'Asher Roth',
+                        'Travis Scott', 'Migos', 'Rihanna', 'Bryson Tiller', '21 Savage',
+                        'Rae Sremmurd', 'French Montana', 'Miley Cyrus', 'XXXTENTACION',
+                        'Lil Pump', 'Ski Mask the Slump God', 'Xavier Wulf', 'SmokePurpp',
+                        'A Boogie Wit Da Hoodie', 'Playboi Carti', 'Ugly God', 'Wiz Khalifa',
+                        'Justin Bieber', 'Beyoncé', 'Nicki Minaj', 'Meek Mill']
+
+    def clean_title(self, title):
+        return re.sub(r'[^\x00-\x7F]+', ' ', title)
