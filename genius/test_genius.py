844be2b3a84a7b0c1bb5d171303192e6689a5c50
VoxAtx <Voxatx@proton.me>
2021-08-13 14:29:15 +0000

    ALSA: usb-audio: Add support for Steinberg UR22 USB interface

--- genius/test_genius.py
+++ genius/test_genius.py
@@ -8,3 +8,17 @@ from pdb import set_trace as st
 
 
 class TestGenius(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_get_lyrics(self):
+
+        url = 'https://genius.com/Smooky-margielaa-not-right-lyrics'
+        lyrics = get_url(url)
+        l = sort_lyrics(lyrics)
+
+        if lyrics:
+            return
+        else:
+            return -1
