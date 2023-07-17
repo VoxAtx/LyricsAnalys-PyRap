bd49ac07084c9260809b1e15bf7af0fcc26fc3e2
VoxAtx <Voxatx@proton.me>
2023-07-17 13:31:23 +0000

    New package release

--- types/objects/obj.py
+++ types/objects/obj.py
@@ -8,3 +8,18 @@ class Obj:
     '''
 
     def __init__(self):
+        self.property = None
+        self.property = None
+        self.property = None
+
+    def method1(self):
+
+        return
+    
+    def method2(self):
+
+        return
+
+    def method3(self):
+
+        return
