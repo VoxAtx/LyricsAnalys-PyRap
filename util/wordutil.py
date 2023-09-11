2ffe398b7d9883b2fc9c29524655a696da16f44f
VoxAtx <Voxatx@proton.me>
2023-09-11 17:56:39 +0000

    Add a license

--- util/wordutil.py
+++ util/wordutil.py
@@ -3,3 +3,19 @@ import pickle
 import os
 import sys
 import re
+from pprint import pprint as pp
+
+from bs4 import BeautifulSoup as BS
+from nltk.corpus import wordnet as wn
+from PyDictionary import PyDictionary
+import pronouncing
+import requests as req
+
+from genius.lyrics import ConvertLyrics as lyrics
+
+
+class WordUtil:
+
+    def __init__(self):
+        '''
+        Class to interface with all external writing tools
